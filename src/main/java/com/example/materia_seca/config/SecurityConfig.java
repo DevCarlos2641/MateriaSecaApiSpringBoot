4e601078cfabef88617336b06f69eddab3cdddb0
DevCarlos2641 <dev.carlos2641@outlook.com>
2025-09-06 19:59:02 -0600

    update

--- src/main/java/com/example/materia_seca/config/SecurityConfig.java
+++ src/main/java/com/example/materia_seca/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -54,8 +55,8 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance(); // in developer
-        //return new BCryptPasswordEncoder(); // Production
+//        return NoOpPasswordEncoder.getInstance(); // in developer
+        return new BCryptPasswordEncoder(); // Production
     }
 
     /*@Bean
--- src/test/java/com/example/materia_seca/MateriaSecaApplicationTests.java
+++ src/test/java/com/example/materia_seca/MateriaSecaApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.materia_seca;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MateriaSecaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
